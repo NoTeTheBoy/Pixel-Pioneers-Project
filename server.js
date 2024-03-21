b9aa070c59c58c6cfb2cb8966edf1ce58c0d6e9b
NoTeTheBoy <noahgottlieb@hotmail.com>
2024-03-21 09:28:07 +0100

    fixed syntax in server.js

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const request = require('request');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
 app.get(`/maps/api/directions/json?destination=${destinationInput.value}&mode=walking&origin=${originInput.value}&key=AIzaSyB8xI3vA3bcGOo7cNG7SWy6GQyIDGt6HcE`, (req, res) => {
   request(
-    { url: `https://maps.googleapis.com/maps/api/directions/json?destination=${destinationInput.value}&mode=walking&origin=${originInput.value}&key=AIzaSyB8xI3vA3bcGOo7cNG7SWy6GQyIDGt6HcE`},
+    {url: `https://maps.googleapis.com/maps/api/directions/json?destination=${destinationInput.value}&mode=walking&origin=${originInput.value}&key=AIzaSyB8xI3vA3bcGOo7cNG7SWy6GQyIDGt6HcE`},
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
       }
-
-      res.json(JSON.parse(body));
+       res.json(JSON.parse(body));
     }
   )
 });
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
